9284678ecb1bb0e20e9661774b3b325aa5251ae9
ManNjoro <elijohnmwoho@gmail.com>
2024-01-04 11:37:59 +0300

    Update `0-validate_utf8.py` and `README.md`

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ Script that determines if a given data set represents a valid UTF-8 encoding
 
 def validUTF8(data):
     """
-    Determine if a given data set represents a valid UTF-8 encoding.
+    Checks if a list of integers represents a valid UTF-8 encoding.
 
     Args:
     - data (list of int): List of integers representing 8-bit data bytes.
@@ -15,32 +15,57 @@ def validUTF8(data):
     - bool: True if data is a valid UTF-8 encoding, else return False.
     """
 
-    def check_next_bytes(data, start, n):
-        for i in range(start + 1, start + n + 1):
-            if i >= len(data) or (data[i] >> 6) != 0b10:
+    skip = 0
+    n = len(data)
+
+    for i in range(n):
+        if skip > 0:
+            if data[i] >> 6 != 0b10:
                 return False
-        return True
-
-    i = 0
-    while i < len(data):
-        byte = data[i]
-
-        if byte >> 7 == 0:
-            i += 1
+            skip -= 1
             continue
 
-        elif byte >> 5 == 0b110 and check_next_bytes(data, i, 1):
-            i += 2
-            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
 
-        elif byte >> 4 == 0b1110 and check_next_bytes(data, i, 2):
-            i += 3
-            continue
-
-        elif byte >> 3 == 0b11110 and check_next_bytes(data, i, 3):
-            i += 4
-            continue
-
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11110000 == 0b11100000:
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
         else:
             return False
 
--- 0x04-utf8_validation/README.md
+++ 0x04-utf8_validation/README.md
@@ -9,4 +9,27 @@ Write a method that determines if a given data set represents a valid UTF-8 enco
 - Each integer represents 1 byte of data, therefore you - only need to handle the 8 least significant bits of each integer
 Example:
 ```
+└─$ cat 0-main.py             
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+validUTF8 = __import__('0-validate_utf8').validUTF8
+
+data = [65]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
+```
+Output:
+```
+└─$ ./0-main.py
+True
+True
+False
 ```
